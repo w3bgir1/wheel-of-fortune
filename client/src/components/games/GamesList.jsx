4c8ca418f7c8170423da5906323900b97b6bbcfc
w3bgir1 <akulshyna@gmail.com>
2019-03-26 12:16:37 +0100

    init commit

--- client/src/components/games/GamesList.jsx
+++ client/src/components/games/GamesList.jsx
@@ -5,7 +5,9 @@ import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper'
-import Card, { CardActions, CardContent } from '@material-ui/core/Card'
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography'
 import './GamesList.css'
 
